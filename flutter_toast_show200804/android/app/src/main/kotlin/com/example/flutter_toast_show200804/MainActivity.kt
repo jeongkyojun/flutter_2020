8fab831d9fa87af8af43331596ff76fcab5489d3
jeongkyojun <kyojun2002@naver.com>
2020-08-04 22:06:07 +0900

    input toast show

---
+++ flutter_toast_show200804/android/app/src/main/kotlin/com/example/flutter_toast_show200804/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_toast_show200804
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_toast_show200804/lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:toast/toast.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Stack(
+          //alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              width:128,
+              height:128,
+              color:Colors.red,
+            ),
+            Container(
+              width:64,
+              height:128,
+              color:Colors.orange,
+            ),
+            Container(
+              width:64,
+              height:64,
+              color:Colors.yellow,
+            ),
+            Container(
+              width:64,
+              height:32,
+              color:Colors.green,
+            ),
+            Container(
+              width:32,
+              height:32,
+              color:Colors.blue,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
